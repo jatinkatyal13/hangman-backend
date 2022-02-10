e26e19c02d8871b267096ce18ab18cb3cb65f8a1
Jatin Katyal <jatin.katyal13@gmail.com>
2022-02-11 04:23:23 +0530

    Added CORS config

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ async function initialize() {
   const app = Express();
   
   app.use(Express.json());
+  app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+    res.set("Access-Control-Allow-Headers", "Content-type")
+    next();
+  })
   app.use("/api", Router);
+
+  app.get("/cookie", (req, res) => {
+    res.setHeader("Set-Cookie", "name=jatin")
+
+    res.send("Hello World")
+  })
 
   await sequelize.sync();
 
